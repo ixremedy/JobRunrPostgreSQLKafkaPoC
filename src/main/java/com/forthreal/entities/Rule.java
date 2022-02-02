e69961155fc92c8501690ac87c4964ea8264c3c6
=Lev Vanyan <lev.vanyan@forthreal.com>
2022-02-02 10:32:47 +0200

    Added fields to rule

--- src/main/java/com/forthreal/entities/Rule.java
+++ src/main/java/com/forthreal/entities/Rule.java
@@ -3,15 +3,22 @@ package com.forthreal.entities;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import javax.persistence.*;
+import java.sql.Time;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity(name = "rule")
+@Entity
 @Getter
 @Setter
 @RequiredArgsConstructor
+@Table(name = "rule")
 public class Rule {
     @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "launch_time")
+    private Time launchTime;
+
+    @Column(name = "day_number")
+    private Integer dayNumber;
 }
