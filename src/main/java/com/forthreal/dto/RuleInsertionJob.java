8313e01516215af7d83413e525e79e5877e3bfef
=Lev Vanyan <lev.vanyan@forthreal.com>
2022-02-02 20:12:02 +0200

    Cosmetic changes in RuleInsertionJob

--- src/main/java/com/forthreal/dto/RuleInsertionJob.java
+++ src/main/java/com/forthreal/dto/RuleInsertionJob.java
@@ -1,21 +1,18 @@
 package com.forthreal.dto;
 
 import com.forthreal.handlers.RuleInsertionJobHandler;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.jobrunr.jobs.lambdas.JobRequest;
-import java.util.UUID;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class RuleInsertionJob implements JobRequest {
-
-    public RuleInsertionJob()
-    {
-        description = "Rule: " + UUID.randomUUID();
-    }
-
     private String description;
+    private LocalDateTime executionTime;
 
     @Override
     public Class<RuleInsertionJobHandler> getJobRequestHandler() {
