d68ecca45646e1e8aa050695a2d82e3de84fa267
=Lev Vanyan <lev.vanyan@forthreal.com>
2022-02-02 20:09:43 +0200

    Added findAllByDayNumber()

--- src/main/java/com/forthreal/repository/IRuleRepository.java
+++ src/main/java/com/forthreal/repository/IRuleRepository.java
@@ -3,7 +3,9 @@ package com.forthreal.repository;
 import com.forthreal.entities.Rule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface IRuleRepository extends JpaRepository<Rule, Long> {
+    List<Rule> findAllByDayNumber(Integer dayNumber);
 }
