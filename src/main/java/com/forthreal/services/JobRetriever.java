a6c881451ce44b31285717e5cb6229bc9cd031c8
=Lev Vanyan <lev.vanyan@forthreal.com>
2022-02-02 20:10:39 +0200

    JobRetriever can do real job scheduling

--- src/main/java/com/forthreal/services/JobRetriever.java
+++ src/main/java/com/forthreal/services/JobRetriever.java
@@ -1,16 +1,34 @@
 package com.forthreal.services;
 
+import com.forthreal.dto.RuleInsertionJob;
 import com.forthreal.repository.IRuleRepository;
 import lombok.AllArgsConstructor;
+import org.jobrunr.scheduling.BackgroundJobRequest;
 import org.jobrunr.scheduling.JobScheduler;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-@AllArgsConstructor
+@AllArgsConstructor()
 public class JobRetriever {
     private IRuleRepository ruleRepository;
     private JobScheduler jobScheduler;
 
-    public void enqueueFromDb()
+    public long enqueueFromDb()
     {
-        ruleRepository.findAll();
+        var dayNumber = LocalDate.now().getDayOfMonth();
+
+        return ruleRepository
+                .findAllByDayNumber(dayNumber)
+                .stream()
+                .map( rule -> {
+                    var time = rule.getLaunchTime();
+                    var hours = time.getHours();
+                    var minutes = time.getMinutes();
+                    var localDateTime = LocalDateTime.now().withHour(hours).withMinute(minutes);
+                    var ruleInsertionJob = new RuleInsertionJob(rule.getDescription(),localDateTime);
+                    return ruleInsertionJob;
+                } )
+                .map( BackgroundJobRequest::enqueue )
+                .count();
     }
 }
